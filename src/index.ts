3b4f6dac1d22258df81d061963e798a45609dff8
fancyzhong <fancyzhong@tencent.com>
2021-02-20 19:31:35 +0800

    chore: update package.json main value

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,8 @@
 import {
   useConcent, IRefCtxM, ModuleDesc,
   ReducerCallerParams, IReducerFn, IActionCtxBase,
-  ICtxBase, IAnyObj, SettingsType, ComputedValType,
+  ICtxBase, IAnyObj, SettingsType, ComputedValType, ComputedValTypeForFn,
+  MultiComputedFn, MultiComputed,
 } from 'concent';
 
 /**
@@ -35,16 +36,80 @@ export function makeUseModel<RootInfo, ModelDesc extends ModuleDesc>(moduleName:
   }
 }
 
-const noop = () => { };
+
+export interface BaseOpts<P extends IAnyObj, Extra extends IAnyObj> {
+  props?: P;
+  extra?: Extra;
+  /** 
+   * 是否透传 cuSpec 给 useConcent函数，默认为true，
+   * 表示需要透传，此时用户不需要再setup函数体内调用 ctx.computed(cuSpec)
+   * 如果用户设置passCuSpec为false，表示传入 cuSpec 仅为了方便推导出refComputed类型，但不透传 cuSpec 给 useConcent函数
+   * 注意此时用户需要在 setup函数体内调用 ctx.computed(cuSpec) 来完成示例计算函数的定义，
+   * 否则 refComputed 里拿不到真正的计算结果
+   */
+  passCuDesc?: boolean;
+}
+
+export interface Opts<CuDesc extends MultiComputed, P extends IAnyObj, Extra extends IAnyObj>
+  extends BaseOpts<P, Extra> {
+  /**
+   * 对象型计算函数描述体
+   *  const cuDesc = {
+   *    xxx:({num}:St)=><button>{num}</button>,
+   *    yyy:({bigNum}:St)=><button>{bigNum}</button>,
+   *  }
+   */
+  cuDesc?: CuDesc;
+}
+export interface CufOpts<CuDesc extends MultiComputedFn, P extends IAnyObj, Extra extends IAnyObj>
+  extends BaseOpts<P, Extra> {
+  /**
+   * 函数型计算函数描述体, 通常用于脱离了setup函数体内时，需要拿到渲染上下文句柄做一些其他事情
+   * 如获得 ctx.mr 去绑定onClick事件等
+   * 
+   *  function cuDesc(ctx:CtxPre){
+   *    const { mr } = ctx;
+   *    return {
+   *      xxx:({num}:St)=><button onClick={mr.add}>{num}</button>
+   *    }
+   *  }
+   */
+  cuDesc?: CuDesc;
+}
+
+export type ValidSetup = (ctx: ICtxBase) => IAnyObj | void;
+export type StepParam = ValidSetup | null;
+
+const noop = () => ({});
+
 export function makeUseModelWithSetup<RootInfo, ModelDesc extends ModuleDesc>(moduleName: string) {
   return function useModelWithSetup<
-    Setup extends (ctx: ICtxBase) => IAnyObj, CuSpec extends IAnyObj, P extends IAnyObj, Extra extends IAnyObj,
-    >(setup: Setup, opts?: { cuSpec?: CuSpec, props?: P, extra?: Extra }) {
-    const { cuSpec, props, extra } = opts || {};
+    Setup extends StepParam, CuDesc extends MultiComputed, P extends IAnyObj, Extra extends IAnyObj,
+    >(setup: Setup, opts?: Opts<CuDesc, P, Extra>) {
+    const { cuDesc, props, extra, passCuDesc = true } = opts || {};
     const targetProps = (props || {}) as P;
-    // @ts-ignore
-    noop(cuSpec);// 仅用于推导出类型
-    type Ctx = IRefCtxM<RootInfo, P, ModelDesc, SettingsType<Setup>, ComputedValType<CuSpec>, Extra>;
-    return useConcent<P, Ctx>({ module: moduleName, setup, props: targetProps, extra });
+    const targetCuDesc = passCuDesc ? cuDesc : null;
+    const targetSetup = (setup || noop) as ValidSetup;
+
+    type Ctx = IRefCtxM<RootInfo, P, ModelDesc, SettingsType<Setup>, ComputedValType<CuDesc>, Extra>;
+    return useConcent<P, Ctx>({ module: moduleName, setup: targetSetup, props: targetProps, extra, cuDesc: targetCuDesc });
+  }
+}
+
+/**
+ * 函数型 cuSpec 需要用此函数来推导类型
+ * @param moduleName
+ */
+export function makeUseModelWithSetupCuf<RootInfo, ModelDesc extends ModuleDesc>(moduleName: string) {
+  return function useModelWithSetupCuf<
+    Setup extends StepParam, CuDesc extends MultiComputedFn, P extends IAnyObj, Extra extends IAnyObj,
+    >(setup: Setup, opts?: CufOpts<CuDesc, P, Extra>) {
+    const { cuDesc, props, extra, passCuDesc = true } = opts || {};
+    const targetProps = (props || {}) as P;
+    const targetCuDesc = passCuDesc ? cuDesc : null;
+    const targetSetup = (setup || noop) as ValidSetup;
+
+    type Ctx = IRefCtxM<RootInfo, P, ModelDesc, SettingsType<Setup>, ComputedValTypeForFn<CuDesc>, Extra>;
+    return useConcent<P, Ctx>({ module: moduleName, setup: targetSetup, props: targetProps, extra, cuDesc: targetCuDesc });
   }
 }
